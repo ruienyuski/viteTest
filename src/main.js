f2f25984a9faabfeadb7fb4333d9a61eb0f49bb4
ruienyuski <yuski.tw@gmail.com>
2023-08-18 14:43:22 +0800

    git init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './assets/main.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueAxios, axios)
+
+app.mount('#app')
